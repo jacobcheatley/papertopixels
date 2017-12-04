749be0c2ec50576ec520f78eb93afbecdd495078
Jacob Cheatley <jacobcheatley@gmail.com>
2017-12-04 14:05:23 +1300

    Attempted to fix bugs, gave up and got stuff working instead

--- image/rectangle.py
+++ image/rectangle.py
@@ -25,10 +25,10 @@ def edges_highlight_rect_ratio(img):
     edges = cv2.Canny(gray, 10, CANNY)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (MORPH, MORPH))
     closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-    _, contours, h = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, _ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     rect = None
-    ratio = 1.5
+    ratio = 1.4142
 
     # Find a rectangle
     # TODO: Largest rectangle
@@ -38,10 +38,16 @@ def edges_highlight_rect_ratio(img):
 
         if len(approx) == 4:
             pts_src = np.array(approx, np.float32)
+
+            if np.argmin(np.sum(pts_src.squeeze(), axis=1)) != 0:
+                # If first point is not top left, must be top right
+                pts_src = np.roll(pts_src, 3, axis=0)
+
             h, status = cv2.findHomography(pts_src, pts_dst)
             rect = cv2.warpPerspective(img, h, (int(WIDTH + MARGIN * 2), int(HEIGHT + MARGIN * 2)))
             cv2.drawContours(img, [approx], -1, (255, 0, 0), 2)
-            ratio = approximate_ratio(approx, img.shape[1], img.shape[0])
+            # ratio = approximate_ratio(pts_src, img.shape[1], img.shape[0])
+            # TODO: Disabled for now, having problems with ratio calculations
             break
 
     return edges, img, rect, ratio
@@ -55,8 +61,12 @@ def approximate_ratio(points, w, h):
     u0 = w / 2
     v0 = h / 2
     vert = (points - np.array([u0, v0])).squeeze()
-    # bl, br, tl, tr -> ATM this is not right
-    m0, m1, m2, m3 = np.append(vert, [[1], [1], [1], [1]], axis=1)
+    # Expected order: bl, br, tl, tr
+    m = np.append(vert, [[1], [1], [1], [1]], axis=1)
+    # Order must be tl, bl, br, tr coming from pts finding
+    print(m)
+    m2, m0, m1, m3 = m
+    print('bl', m0, 'br', m1, 'tl', m2, 'tr', m3)
 
     k2 = np.dot(np.cross(m0, m3), m2) / np.dot(np.cross(m1, m3), m2)
     k3 = np.dot(np.cross(m0, m3), m1) / np.dot(np.cross(m2, m3), m1)
@@ -80,8 +90,8 @@ def approximate_ratio(points, w, h):
     a_inv = np.linalg.inv(a)
     at_inv = np.linalg.inv(a.transpose())
 
-    return 1 / sqrt(((n2 @ at_inv) @ (a_inv @ np.transpose(n2))) /
-                    ((n3 @ at_inv) @ (a_inv @ np.transpose(n3))))
+    return sqrt(((n2 @ at_inv) @ (a_inv @ np.transpose(n2))) /
+                ((n3 @ at_inv) @ (a_inv @ np.transpose(n3))))
 
 
 def rectangle_preview(img):
