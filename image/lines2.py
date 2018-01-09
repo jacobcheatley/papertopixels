85a677947f08cba7d3fd1564d50700c99ea2f5a1
Jacob Cheatley <jacobcheatley@gmail.com>
2018-01-10 11:23:26 +1300

    Set up for segment joining

--- image/lines2.py
+++ image/lines2.py
@@ -2,16 +2,33 @@ import numpy as np
 import cv2
 
 NEIGH = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+MIN_COMP = 10
+
+
+def join_segments(segments):
+    """Figure out how all the segments should join, then return all simplified lines"""
+    # First, join up everything with EXACTLY ONE partner into new lines
+    # While there are still lines that can be joined, test combinations for loop creation, then longest line
+    # When there are no more matching ends, return all joined lines
+    print('COMPONENT')
+    finished = []
+    for seg in segments:
+        print('SEG-INFO', seg[0], seg[-1], len(seg))
+    return finished
 
 
 def get_all_lines2(*img_and_labels):
+    """From each color channel, yield out all the labelled lines"""
     for img, label in img_and_labels:
-        visited = np.zeros(img.shape, np.bool_)
+        print(label)
+        visited = set()
 
         def exhaust(y_s, x_s):
+            """Exhaust an entire component to extract partial segments"""
             segments = []
             branch_points = [(y_s, x_s)]  # To keep track of unfinished branches / joins
             terminals = set()  # To keep track of all branch points / joins
+            comp_pixels = [0]  # To keep track of total component size. Have to keep in list to assign, gross
 
             def neighbors(pix):
                 unvisit = []
@@ -20,7 +37,7 @@ def get_all_lines2(*img_and_labels):
                 for d in NEIGH:
                     n_y, n_x = pix[0] + d[0], pix[1] + d[1]
                     if img[n_y, n_x]:
-                        if not visited[n_y, n_x]:
+                        if (n_y, n_x) not in visited:
                             unvisit.append((n_y, n_x))
                         elif (n_y, n_x) in terminals:
                             term.append((n_y, n_x))
@@ -43,6 +60,11 @@ def get_all_lines2(*img_and_labels):
                         return test
                 return None
 
+            def visit(pix):
+                if pix not in visited:
+                    comp_pixels[0] += 1
+                    visited.add(pix)
+
             # Keep going back while there are unfinished branches in this component
             while True:
                 curr = get_start_point()
@@ -56,7 +78,7 @@ def get_all_lines2(*img_and_labels):
                     current_segment.clear()
 
                 while True:
-                    visited[curr[0], curr[1]] = True
+                    visit(curr)
                     current_segment.append(curr)
                     neigh, terms = neighbors(curr)
 
@@ -88,26 +110,33 @@ def get_all_lines2(*img_and_labels):
                             if highest > adj:
                                 # Use the NEXT point as branch point
                                 curr = neigh[counts.index(highest)]
-                                visited[curr[0], curr[1]] = True
+                                visit(curr)
                                 current_segment.append(curr)
                             branch_points.append(curr)
                             terminals.add(curr)
                             end_segment()
                             break
 
-            print('COMPONENT\nseg: ' + '\nseg: '.join(str(l) for l in segments))
+            return segments if comp_pixels[0] > MIN_COMP else None
 
         # Search for search start locations
         for y in range(img.shape[0]):
             # TODO: Faster iteration tests
             for x in range(img.shape[1]):
-                if visited[y, x]:
+                if (y, x) in visited:
                     continue
                 elif img[y, x]:
-                    exhaust(y, x)
-        print('END')
+                    segments = exhaust(y, x)
+                    # Segments is none if the total shape is too small
+                    if segments is not None:
+                        for points, closed in join_segments(segments):
+                            yield {
+                                'color': label,
+                                'closed': closed,
+                                'points': [{'x': x, 'y': y} for x, y in points.tolist()],
+                            }
 
 
 if __name__ == '__main__':
-    test = cv2.imread('out/17/lines/k.png', cv2.IMREAD_GRAYSCALE)
-    get_all_lines2((test, 'test-red'))
+    test = cv2.imread('out/17/lines/g.png', cv2.IMREAD_GRAYSCALE)
+    list(get_all_lines2((test, 'test-red')))
